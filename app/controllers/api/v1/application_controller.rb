20a9f2fbb8bef5a6445de0a9b4ac3f9b34ecc750
Mário Adriano <mariosergioadriano@gmail.com>
2023-05-17 22:02:55 -0300

    Creating category endpoint

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,10 @@
+# typed: true
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ApplicationController < ActionController::API
+      extend T::Sig
+    end
+  end
+end
---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,65 @@
+# typed: true
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CategoriesController < Api::V1::ApplicationController
+      before_action :set_category, only: [:show, :update, :destroy]
+
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+      # GET /categories
+      def index
+        @categories = Category.all
+        render(json: @categories)
+      end
+
+      # GET /categories/1
+      def show
+        render(json: @category)
+      end
+
+      # POST /categories
+      def create
+        @category = Category.new(category_params)
+
+        if @category.save
+          render(json: @category, status: :created)
+        else
+          render(json: @category.errors, status: :unprocessable_entity)
+        end
+      end
+
+      # PATCH/PUT /categories/1
+      def update
+        if @category.update(category_params)
+          render(json: @category)
+        else
+          render(json: @category.errors, status: :unprocessable_entity)
+        end
+      end
+
+      # DELETE /categories/1
+      def destroy
+        @category = Category.find(params[:id])
+      end
+
+      private
+
+      sig { returns(ActionController::Parameters) }
+      def category_params
+        params.require(:category).permit(:name)
+      end
+
+      sig { returns(Category) }
+      def set_category
+        @category = Category.find(params[:id])
+      end
+
+      sig { returns(T.untyped) }
+      def record_not_found
+        render(json: { error: "Record not found" }, status: :not_found)
+      end
+    end
+  end
+end
