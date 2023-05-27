a13e225990f65a24d31f12baa2a24ced75d6535b
Mário Adriano <mariosergioadriano@gmail.com>
2023-05-27 15:43:14 -0300

    Adjusting the book model test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -42,66 +42,66 @@ RSpec.describe(Book, type: :model) do
   end
 
   describe "validations" do
-    before do
-      @book = build(:book)
-    end
+    # before do
+    #   @book = build(:book)
+    # end
 
-    context "when validating title" do
-      it "is invalid without a title" do
-        @book.title = ""
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:title]).to(include({ error: :blank }))
+    subject { book }
+
+    let(:book) { build(:book) }
+
+    context "when title is invalid" do
+      before { book.title = title }
+
+      context "when title is empty" do
+        let(:title) { "" }
+
+        it { is_expected.to(be_invalid) }
+        it { is_expected.to(validate_presence_of(:title)) }
       end
 
-      it "is invalid with a title longer than 70 characters" do
-        @book.title = "a" * 71
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:title]).to(include({ count: 70, error: :too_long }))
-      end
-    end
+      context "when title is too long" do
+        let(:title) { "a" * 71 }
 
-    context "when validating description" do
-      it "is invalid without a description" do
-        @book.description = ""
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:description]).to(include({ error: :blank }))
-      end
-
-      it "is invalid with a description longer than 500 characters" do
-        @book.description = "a" * 501
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:description]).to(include({ count: 500, error: :too_long }))
+        it { is_expected.to(be_invalid) }
+        it { is_expected.to(validate_length_of(:title).is_at_most(70)) }
       end
     end
 
-    context "when validating original_publication_year" do
-      it "is invalid without a publication year" do
-        @book.original_publication_year = nil
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:original_publication_year]).to(include({ error: :not_a_number, value: nil }))
+    context "when description is invalid" do
+      before { book.description = description }
+
+      context "when description is empty" do
+        let(:description) { "" }
+
+        it { is_expected.to(be_invalid) }
+        it { is_expected.to(validate_presence_of(:description)) }
       end
 
-      it "is invalid with a non-numeric publication year" do
-        @book.original_publication_year = "abc"
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:original_publication_year]).to(include({ error: :not_a_number, value: "abc" }))
-      end
+      context "when description is too long" do
+        let(:description) { "a" * 501 }
 
-      it "is invalid with a publication year in the future" do
-        @book.original_publication_year = Time.current.year + 1
-        expect(@book).to(be_invalid)
-        expect(@book.errors.details[:original_publication_year]).to(include({
-          count: 2023,
-          error: :less_than_or_equal_to,
-          value: 2024,
-        }))
+        it { is_expected.to(be_invalid) }
+        it { is_expected.to(validate_length_of(:description).is_at_most(500)) }
+      end
+    end
+
+    context "when original_publication_year is invalid" do
+      before { book.original_publication_year = original_publication_year }
+
+      context "when original_publication_year is empty" do
+        let(:original_publication_year) { "" }
+
+        it { is_expected.to(be_invalid) }
+        it {
+          is_expected.to(validate_numericality_of(:original_publication_year)
+          .is_less_than_or_equal_to(Time.current.year))
+        }
       end
     end
 
     context "when all attributes are valid" do
-      it "is valid with valid attributes" do
-        expect(@book).to(be_valid)
-      end
+      it { is_expected.to(be_valid) }
     end
   end
 end
